cdd1d3f4acf7cd9deae30a9d8140165a0388e42e
Tikumporn Wankvar <thesecondstageshow@gmail.com>
2017-09-17 19:55:39 +0700

    Adding Post with id

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,27 +1,59 @@
 import React, { Component } from  'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { createPost } from '../actions'
+
 class PostsNew extends Component{
   renderField(field){
+    const { meta: { touched, error } } = field; // Destructuring
+    const className = `form-group ${error && touched ? 'has-danger' : ''}`
     return(
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           {...field.input}
         />
-        {field.meta.error}
+        <div className="text-help">
+        {touched ? error : ''}
+        </div>
       </div>
     );
   }
+  onSubmit(values){
+    //this === component
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
+  }
   render() {
+    const { handleSubmit } = this.props;
+
       return (
-        <form>
+        <form onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
             <Field
               label="Title"
               name="title"
               component={ this.renderField }
             />
+
+            <Field
+              label="Categories"
+              name="categories"
+              component={ this.renderField }
+            />
+
+            <Field
+              label="Post Content"
+              name="content"
+              component={ this.renderField }
+            />
+
+            <button type="submit" className="btn btn-primary">Submit</button>
+            <Link to="/" className="btn btn-danger">Cancel</Link>
         </form>
       );
   }
@@ -29,9 +61,19 @@ class PostsNew extends Component{
 
 function validate(values){
   const errors = {};
-  return error;
+  if(!values.title){
+    errors.title = "Please Enter Some Text!!";
+  }
+
+  if(!values.categories) {
+      errors.categories = "Please Enter Some Text!!";
+  }
+  return errors;
 }
 
 export default reduxForm({
+  validate,
   form: 'PostNewForm'
-})(PostsNew);
+})(
+  connect(null, {createPost})(PostsNew)
+);
